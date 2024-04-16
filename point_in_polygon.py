2127b7ed25a4f42bc691adec18e62577d8b81564
CyrilGeistlich <cyril@geistlich.com>
2024-04-16 13:52:33 +0200

    import added

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -1,1 +1,20 @@
 # Spatial Algorithm here... 
+
+## Libraries ##
+
+import pandas as pd # Used to import csv
+import geopandas as gpd # Used to import gpkg
+
+
+
+## Import Data ## 
+
+mun = gpd.read_file('data/swissBOUNDARIES3D_1_5_LV95_LN02.gpkg')
+
+
+# The SwissNames3D Gazetteer comes with 3 seperate csv files, all containing points.
+# The names_lin and names_pol are already reduced to points in the downloaded data set.
+
+names_pt = pd.read('data/swissnames3d_2023_2056/swissNAMES3D_PKT.csv')
+names_lin = pd.read('data/swissnames3d_2023_2056/swissNAMES3D_LIN.csv')
+names_pol = pd.read('data/swissnames3d_2023_2056/swissNAMES3D_PLY.csv')
