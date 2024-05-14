86106532f88d039e22394e6eda3762f147035f01
CyrilGeistlich <cyril@geistlich.com>
2024-05-14 09:13:24 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,18 @@
+from classes import *
+from import_json import *
+
+## EXECTUE FUNCTIONS ##
+
+# 1. READ DATA (MAREK)
+
+process_json_file()
+
+# (1.5 SPATIAL INDEX???) (TAO)
+
+# 2. POLYGON CLIPPING (SEBASTIAN)
+
+
+# 3. POINT IN POLYGON (TAO)
+
+# 4. COUNT, ANALYSIS PREPROCESSING (CYRIL)
+
